fb72daaf4c14203f908fbe57cf2ee631fbb14f31
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2024-06-17 10:13:57 -0400

    Add mlcm on model names

--- llm_paper_extract/analyse.py
+++ llm_paper_extract/analyse.py
@@ -1,7 +1,21 @@
 from typing import List
 
-from mlcm import mlcm
-import numpy as np
+try:
+    import numpy as np
+    from mlcm import mlcm
+    np.int
+except AttributeError:
+    # Fix AttributeError in mlcm.mlcm
+    # *** AttributeError: module 'numpy' has no attribute 'int'.
+    # `np.int` was a deprecated alias for the builtin `int`. To avoid this error
+    # in existing code, use `int` by itself. Doing this will not modify any
+    # behavior and is safe. When replacing `np.int`, you may wish to use e.g.
+    # `np.int64` or `np.int32` to specify the precision. If you wish to review
+    # your current use, check the release note link for additional information.
+    # The aliases was originally deprecated in NumPy 1.20; for more details and
+    # guidance see the original release note at:
+    #     https://numpy.org/devdocs/release/1.20.0-notes.html#deprecations. Did you mean: 'inf'?
+    np.int = int
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 
@@ -44,12 +58,6 @@ def _mlcm(annotations:pd.DataFrame, predictions:pd.DataFrame):
         [classes.isin(arr).astype(int) for arr in predictions]
     )
 
-    # Fix
-    # *** AttributeError: module 'numpy' has no attribute 'int'.
-    # `np.int` was a deprecated alias for the builtin `int`. To avoid this error in existing code, use `int` by itself. Doing this will not modify any behavior and is safe. When replacing `np.int`, you may wish to use e.g. `np.int64` or `np.int32` to specify the precision. If you wish to review your current use, check the release note link for additional information.
-    # The aliases was originally deprecated in NumPy 1.20; for more details and guidance see the original release note at:
-    #     https://numpy.org/devdocs/release/1.20.0-notes.html#deprecations. Did you mean: 'inf'?
-    np.int = int
     return mlcm.cm(_ann, _pred)
 
 
@@ -93,7 +101,7 @@ if __name__ == "__main__":
 
     for label in ("title", "type", "research_field"):
         for i in range(2):
-            _mat = confusion_matrix(
+            mat = confusion_matrix(
                 annotated[0].loc[:,label],
                 predictions[0].loc[:,i,:].loc[:,label]
             )
@@ -101,32 +109,57 @@ if __name__ == "__main__":
             # if label == "title":
             #     assert (_mat == np.identity(_mat.shape[0])).all()
 
-            print(f"{label}:", _mat, sep="\n")
+            print(f"{label}:", mat, sep="\n")
 
     for label in ("sub_research_field","all_research_fields",):
         for i in range(2):
-            _ann, _pred = (
+            ann, pred = (
                 annotated[0].loc[:,label],
                 predictions[0].loc[:,i,:].loc[:,label]
             )
 
-            conf_mat, normal_conf_mat = _mlcm(_ann, _pred)
+            conf_mat, normal_conf_mat = _mlcm(ann, pred)
             print(f"{label}:")
             print("Raw confusion Matrix:")
             print(conf_mat)
             print("Normalized confusion Matrix (%):")
             print(normal_conf_mat)
 
-    # for label in ("models","datasets","libraries",):
-    #     for i in range(2):
-    #         _ann, _pred = (
-    #             annotated[1].loc[:,label,:],
-    #             predictions[1].loc[:,i,label,:]
-    #         )
+    for label in ("models","datasets","libraries",):
+        for i in range(2):
+            ann, pred = (
+                annotated[1].loc[:,label,:],
+                predictions[1].loc[:,i,label,:]
+            )
+            ann_papers, pred_papers = (
+                set(ind[0] for ind in ann.index),
+                set(ind[0] for ind in pred.index)
+            )
+            ann_per_paper, pred_per_paper = (
+                [
+                    ann.loc[p,:]
+                    if p in ann_papers
+                    else pd.DataFrame([], columns=ann.columns)
+                    for p in sorted(ann_papers.union(pred_papers))
+                ],
+                [
+                    pred.loc[p,:]
+                    if p in pred_papers
+                    else pd.DataFrame([], columns=pred.columns)
+                    for p in sorted(ann_papers.union(pred_papers))
+                ],
+            )
 
-    #         conf_mat, normal_conf_mat = _mlcm(_ann, _pred)
-    #         print(f"{label}:")
-    #         print("Raw confusion Matrix:")
-    #         print(conf_mat)
-    #         print("Normalized confusion Matrix (%):")
-    #         print(normal_conf_mat)
+            for col in ann.columns:
+                if col == "name":
+                    conf_mat, normal_conf_mat = _mlcm(
+                        [_ann.loc[:,col] for _ann in ann_per_paper],
+                        [_pred.loc[:,col] for _pred in pred_per_paper],
+                    )
+                    print(f"{label}.{col}:")
+                    print("Raw confusion Matrix:")
+                    print(conf_mat)
+                    print("Normalized confusion Matrix (%):")
+                    print(normal_conf_mat)
+                else:
+                    pass
