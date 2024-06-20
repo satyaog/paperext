8f08500faa2d6457480286ae96aaab769f58e54f
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2024-06-20 14:16:28 -0400

    new model version

--- llm_paper_extract/models/model.py
+++ llm_paper_extract/models/model.py
@@ -6,8 +6,6 @@ from typing import Any, Generic, List, Optional, TypeVar
 
 from pydantic import BaseModel, Field
 
-from .. import ROOT_DIR
-
 logging.basicConfig(level=logging.DEBUG)
 
 _FIRST_MESSAGE = (
@@ -22,66 +20,22 @@ _RETRY_MESSAGE = (
     "{}\n"
     "your precedent list of Libraries\n"
     "{}\n"
-    "please find more Deep Learning Models, Datasets and Libraries in the "
+    "which might be incomplete or erroneous, please find more Deep Learning Models, Datasets and Libraries in the "
     "same research paper:\n"
     "{}"
 )
 _EMPTY_FLAG = "__EMPTY__"
 
 
-# def _caseinsensitive_missing_(cls:enum.Enum, value):
-#     # import ipdb ; ipdb.set_trace()
-#     if isinstance(value, str):
-#         print(f"in:{value}")
-#         value = str_normalize(value.split()[0])
-#         print(f"out:{value}")
-#     for member in cls:
-#         if member.lower() == value:
-#             print(f"out:{member}")
-#             return member
-#     # try:
-#     #     # Counting on the string version to save us here
-#     #     value = max(0, int(value) - 1)
-#     #     if value < 0:
-#     #         raise IndexError
-#     #     return list(cls)[value]
-#     # except ValueError:
-#     #     pass
-#     # except IndexError:
-#     #     pass
-#     return None
-
-
-# def _model_incensitive__eq__(self:BaseModel, other):
-#     for (k1,v1), (k2,v2) in zip(self, other):
-#         if k1 != k2:
-#             return False
-#         if isinstance(v1, str) and isinstance(v2, str):
-#             if v1.lower() != v2.lower():
-#                 return False
-#         elif v1 != v2:
-#             return False
-#     else:
-#         return True
-
-
 class ResearchType(str, enum.Enum):
     EMPIRICAL = "empirical"
     THEORETICAL = "theoretical"
 
-    # @classmethod
-    # def _missing_(cls, value):
-    #     return _caseinsensitive_missing_(cls, value)
-
 
 class ModelMode(str, enum.Enum):
     TRAINED = "trained"
-    FINE_TUNED = "fine-tuned"
+    FINETUNED = "finetuned"
     INFERENCE = "inference"
-
-    # @classmethod
-    # def _missing_(cls, value):
-    #     return _caseinsensitive_missing_(cls, value)
 
 
 # TODO: make list of contributed, used, referenced models, datasets and libraries
@@ -90,20 +44,14 @@ class Role(str, enum.Enum):
     USED = "used"
     REFERENCED = "referenced"
 
-    # @classmethod
-    # def _missing_(cls, value):
-    #     return _caseinsensitive_missing_(cls, value)
-
 
 T = TypeVar("T")
 class Explained(BaseModel, Generic[T]):
     value: T | str
-    # value_str: str = Field(description="Literal conversion of the value")
     justification: str = Field(
         description="Short justification for the choice of the value",
     )
     quote: str = Field(
-        # description=f"Short literal quote from the paper on which the choice of the value was made",
         description="The best literal quote from the paper which supports the value",
     )
 
@@ -121,26 +69,21 @@ class Model(BaseModel):
     name: Explained[str] = Field(
         description="Name of the Model",
     )
-    # list of carateristics for the model like convolution layers, transformer modules
-    type: Explained[str] = Field(
-        description="Type of the Model",
+    caracteristics: List[Explained[str]] = Field(
+        description="List of carateristics of the Model like convolution layers, transformer modules",
     )
-    role: Role | str = Field(
-        description=f"Was the Model {' or '.join([role.value.lower() for role in Role])} in the scope of the paper"
+    is_executed: Explained[bool] = Field(
+        description="Was the Model executed on GPU or CPU, in the scope of the paper",
     )
-    mode: ModelMode | str = Field(
-        description=f"Was the Model {' or '.join([mode.value.lower() for mode in ModelMode])} in the scope of the paper"
+    is_compared: Explained[bool] = Field(
+        description="Was the Model compared numerically to other models, in the scope of the paper",
     )
-
-    # @field_validator("role", mode="after")
-    # @classmethod
-    # def normalize(cls, v: str, _: ValidationInfo) -> str:
-    #     return Role(v) or v
-
-    # @field_validator("mode", mode="after")
-    # @classmethod
-    # def normalize(cls, v: str, _: ValidationInfo) -> str:
-    #     return ModelMode(v) or v
+    is_contributed: Explained[bool] = Field(
+        description="Was the Model a contribution to the research field, in the scope of the paper",
+    )
+    referenced_paper_title: Explained[str] = Field(
+        description="Title of the Model reference paper found in the references section",
+    )
 
 
 class Dataset(BaseModel):
@@ -150,11 +93,9 @@ class Dataset(BaseModel):
     role: Role | str = Field(
         description=f"Was the Dataset {' or '.join([role.value.lower() for role in Role])} in the scope of the paper"
     )
-
-    # @field_validator("role", mode="after")
-    # @classmethod
-    # def normalize(cls, v: str, _: ValidationInfo) -> str:
-    #     return Role(v) or v
+    referenced_paper_title: Explained[str] = Field(
+        description="Title of the Dataset reference paper found in the references section",
+    )
 
 
 class Library(BaseModel):
@@ -164,11 +105,9 @@ class Library(BaseModel):
     role: Role | str = Field(
         description=f"Was the Library {' or '.join([role.value.lower() for role in Role])} in the scope of the paper"
     )
-
-    # @field_validator("role", mode="after")
-    # @classmethod
-    # def normalize(cls, v: str, _: ValidationInfo) -> str:
-    #     return Role(v) or v
+    referenced_paper_title: Explained[str] = Field(
+        description="Title of the Library reference paper found in the references section",
+    )
 
 
 class PaperExtractions(BaseModel):
@@ -181,16 +120,11 @@ class PaperExtractions(BaseModel):
     type: Explained[ResearchType] = Field(
         description=f"Is the paper an {' or a '.join([rt.value.lower() + ' study' for rt in ResearchType])}",
     )
-    # Should be renamed to primary research field
-    research_field: Explained[str] = Field(
-        description="Deep Learning research field of the paper",
+    primary_research_field: Explained[str] = Field(
+        description="Primary Deep Learning research field of the paper",
     )
-    # This should have been a list
-    # sub_research_field: List[Explained[str]] | Explained[str] = Field(
-    #     description="List of Deep Learning research sub-fields of the paper",
-    # )
-    sub_research_field: Explained[str] = Field(
-        description="Deep Learning sub-research field of the paper",
+    sub_research_fields: List[Explained[str]] = Field(
+        description="List of Deep Learning sub-research fields and research sub-fields of the paper, order by importance",
     )
     models: List[Model] = Field(
         description="All Models found in the paper"
@@ -201,12 +135,6 @@ class PaperExtractions(BaseModel):
     libraries: List[Library] = Field(
         description="All Deep Learning Libraries found in the paper"
     )
-
-    # @field_validator("type", mode="after")
-    # @classmethod
-    # def normalize(cls, v: str, _: ValidationInfo) -> str:
-    #     v.value = ResearchType(v.value) or v
-    #     return v
 
 
 # PaperExtractions = fix_explained_fields()
