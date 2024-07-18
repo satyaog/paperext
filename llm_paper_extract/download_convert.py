60593c042b1aaece3d7e353188f5c78e0c1083f8
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2024-07-18 10:17:39 -0400

    blackify

--- llm_paper_extract/download_convert.py
+++ llm_paper_extract/download_convert.py
@@ -1,21 +1,23 @@
+from __future__ import annotations
+
 import argparse
 import json
-from pathlib import Path
 import subprocess
 import sys
 import urllib.request
+from pathlib import Path
 
 from .utils import python_module
 
-PROG=f"python3 -m {python_module(__file__)}"
+PROG = f"python3 -m {python_module(__file__)}"
 
-DESCRIPTION="""
+DESCRIPTION = """
 Utility to download and convert a list of papers' pdfs -> "txts.
 
 stdout will contain the list of files successfully converted separated by '\\n'.
 """
 
-EPILOG=f"""
+EPILOG = f"""
 Example:
   $ {PROG} paperoni-2024-07-04.json
     [DEBUG]
@@ -49,7 +51,7 @@ Example:
 def convert_pdf(pdf, text, pdf_link):
     pdf.parent.mkdir(parents=True, exist_ok=True)
     if not pdf.exists():
-        print('Downloading from', pdf_link, 'to', str(pdf), file=sys.stderr)
+        print("Downloading from", pdf_link, "to", str(pdf), file=sys.stderr)
         try:
             urllib.request.urlretrieve(pdf_link, str(pdf))
         except (urllib.error.HTTPError, ValueError) as e:
@@ -60,14 +62,12 @@ def convert_pdf(pdf, text, pdf_link):
         # pdftotext comes from https://poppler.freedesktop.org/
         try:
             cmd = ["pdftotext", str(pdf), str(text)]
-            cp = subprocess.run(
-                cmd,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT
-            )
+            cp = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             print(cp.stdout, file=sys.stderr)
             if cp.returncode:
-                raise subprocess.CalledProcessError(cp.returncode, cmd, cp.stdout, cp.stderr)
+                raise subprocess.CalledProcessError(
+                    cp.returncode, cmd, cp.stdout, cp.stderr
+                )
         except subprocess.CalledProcessError as e:
             print(f"Failed to convert {pdf} to {text}: {e}", file=sys.stderr)
             pdf.unlink(missing_ok=True)
@@ -76,29 +76,29 @@ def convert_pdf(pdf, text, pdf_link):
     return text
 
 
-def download_and_convert_paper(paper_id:str, links:list, cache_dir:Path, check_only=False):
+def download_and_convert_paper(
+    paper_id: str | None, links: list, cache_dir: Path, check_only=False
+):
     text = None
     link_types = []
     while links:
-        l:dict = links.pop(0)
-        link_type = l['type'].split(".")[0]
-        for _if, pdf, pdf_link in (
+        l: dict = links.pop(0)
+        link_type = l["type"].split(".")[0]
+        sources = [
             (
                 l["type"].lower().startswith("arxiv"),
                 cache_dir / f"arxiv/{l['link']}.pdf",
-                f"https://arxiv.org/pdf/{l['link']}"
+                f"https://arxiv.org/pdf/{l['link']}",
             ),
             (
                 "url" in l,
                 cache_dir / link_type / f"{l['link']}.pdf",
-                l.get("url", None)
+                l.get("url", None),
             ),
-            (
-                True,
-                cache_dir / link_type / f"{paper_id}.pdf",
-                l["link"]
-            ),
-        ):
+        ]
+        if paper_id is not None:
+            sources.append((True, cache_dir / link_type / f"{paper_id}.pdf", l["link"]))
+        for _if, pdf, pdf_link in sources:
             if not _if:
                 continue
             text = pdf.with_suffix(".txt")
@@ -125,20 +125,20 @@ def main(argv=None):
         prog=PROG,
         description=DESCRIPTION,
         epilog=EPILOG,
-        formatter_class=argparse.RawDescriptionHelpFormatter
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
         "input",
         metavar="JSON",
         type=Path,
-        help="Paperoni json output of papers to download and convert pdfs -> txts"
+        help="Paperoni json output of papers to download and convert pdfs -> txts",
     )
     parser.add_argument(
         "--cache-dir",
         metavar="DIR",
         type=Path,
         default=Path("data/cache/"),
-        help="Directory to store downloaded and converted pdfs -> txts"
+        help="Directory to store downloaded and converted pdfs -> txts",
     )
     options = parser.parse_args(argv)
 
@@ -150,66 +150,52 @@ def main(argv=None):
     failed = []
     for p in paperoni:
         text = None
-        links = [
-            l for l in p["links"]
-            if l["type"].lower().startswith("arxiv")
-        ]
+        links = [l for l in p["links"] if l["type"].lower().startswith("arxiv")]
         links.extend(
             [
-                l for l in p["links"]
-                if "pdf" in l["type"].lower().split(".") and l["type"].lower().startswith("openreview")
-            ]
-        )
-        links.extend(
-            [
-                l for l in p["links"]
+                l
+                for l in p["links"]
                 if "pdf" in l["type"].lower().split(".")
+                and l["type"].lower().startswith("openreview")
             ]
         )
-        links.extend(
-            [
-                l for l in p["links"]
-                if "pdf" in l["link"].lower()
-            ]
-        )
+        links.extend([l for l in p["links"] if "pdf" in l["type"].lower().split(".")])
+        links.extend([l for l in p["links"] if "pdf" in l["link"].lower()])
         if not links:
-            print(f"Could not find any pdf links for paper {p['links']} in", *p["links"], sep="\n", file=sys.stderr)
+            print(
+                f"Could not find any pdf links for paper {p['links']} in",
+                *p["links"],
+                sep="\n",
+                file=sys.stderr,
+            )
         else:
             print(f'Downloading and converting {p["title"]}', file=sys.stderr)
 
         for check_only in (True, False):
             text, link_types = download_and_convert_paper(
-                p["paper_id"],
-                links[:],
-                options.cache_dir,
-                check_only=check_only
+                p["paper_id"], links[:], options.cache_dir, check_only=check_only
             )
             if text is not None:
-                completed.append((p['paper_id'], text, link_types))
+                completed.append((p["paper_id"], text, link_types))
                 break
         else:
-            failed.append((p['paper_id'], text, link_types))
-            print(f'Failed to download or convert {p["paper_id"]}:{p["title"]} with links {link_types}', file=sys.stderr)
+            failed.append((p["paper_id"], text, link_types))
+            print(
+                f'Failed to download or convert {p["paper_id"]}:{p["title"]} with links {link_types}',
+                file=sys.stderr,
+            )
 
     print(*sorted(str(text) for _, text, _ in completed), sep="\n")
 
     print(
         f"Successfully downloaded and converted {len(completed)} out of "
         f"{len(completed) + len(failed)} papers",
-        file=sys.stderr
+        file=sys.stderr,
     )
     for t in sorted(
-        set(
-            sum(
-                [l for _, _, l in completed] + [l for _, _, l in failed],
-                []
-            )
-        )
+        set(sum([l for _, _, l in completed] + [l for _, _, l in failed], []))
     ):
-        c, f = (
-            sum(t in l for _, _, l in completed),
-            sum(t in l for _, _, l in failed)
-        )
+        c, f = (sum(t in l for _, _, l in completed), sum(t in l for _, _, l in failed))
         print(f"{t}:{c}/{c+f}", file=sys.stderr)
 
 
--- llm_paper_extract/query.py
+++ llm_paper_extract/query.py
@@ -11,30 +11,38 @@ from openai.types.chat.chat_completion import CompletionUsage
 import pydantic_core
 
 from . import ROOT_DIR
-from .models.model import _FIRST_MESSAGE, _RETRY_MESSAGE, ExtractionResponse, PaperExtractions
+from .models.model import (
+    _FIRST_MESSAGE,
+    _RETRY_MESSAGE,
+    ExtractionResponse,
+    PaperExtractions,
+)
 from .utils import build_validation_set, python_module
 
-PROG=f"python3 -m {python_module(__file__)}"
+PROG = f"python3 -m {python_module(__file__)}"
 
-DESCRIPTION="""
+DESCRIPTION = """
 Utility to query Chat-GPT on papers
 """
 
-EPILOG=f"""
+EPILOG = f"""
 Example:
   $ {PROG} --input data/query_set.txt > query.out
 """
 
 
 async def extract_from_research_paper(
-        client: instructor.client.Instructor | instructor.client.AsyncInstructor,
-        message: str,
+    client: instructor.client.Instructor | instructor.client.AsyncInstructor,
+    message: str,
 ) -> Tuple[PaperExtractions, CompletionUsage]:
     """Extract Models, Datasets and Frameworks names from a research paper."""
     retries = [True] * 2
     while True:
         try:
-            extractions, completion = await client.chat.completions.create_with_completion(
+            (
+                extractions,
+                completion,
+            ) = await client.chat.completions.create_with_completion(
                 model="gpt-4o",
                 # model="gpt-3.5-turbo",
                 response_model=PaperExtractions,
@@ -42,16 +50,16 @@ async def extract_from_research_paper(
                     {
                         "role": "system",
                         "content": f"Your role is to extract Deep Learning Models, Datasets and Deep Learning Libraries from a given research paper."
-                                  #  f"The Models, Datasets and Frameworks must be used in the paper "
-                                  #  f"and / or the comparison analysis of the results of the "
-                                  #  f"paper. The papers provided will be a convertion from pdf to text, which could imply some formatting issues.",
+                        #  f"The Models, Datasets and Frameworks must be used in the paper "
+                        #  f"and / or the comparison analysis of the results of the "
+                        #  f"paper. The papers provided will be a convertion from pdf to text, which could imply some formatting issues.",
                     },
                     {
                         "role": "user",
                         "content": message,
                     },
                 ],
-                max_retries=2
+                max_retries=2,
             )
             return extractions, completion.usage
         except openai.RateLimitError as e:
@@ -63,8 +71,8 @@ async def extract_from_research_paper(
 
 
 async def batch_extract_models_names(
-        client: instructor.client.Instructor | instructor.client.AsyncInstructor,
-        papers_fn: List[Path]
+    client: instructor.client.Instructor | instructor.client.AsyncInstructor,
+    papers_fn: List[Path],
 ) -> List[ExtractionResponse]:
     for paper_fn in papers_fn:
         paper = paper_fn.name
@@ -81,10 +89,7 @@ async def batch_extract_models_names(
 
             try:
                 response = ExtractionResponse.model_validate_json(f.read_text())
-            except (
-                FileNotFoundError,
-                pydantic_core._pydantic_core.ValidationError
-            ):
+            except (FileNotFoundError, pydantic_core._pydantic_core.ValidationError):
                 message = message.format(*data, paper_fn.read_text())
 
                 extractions, usage = await extract_from_research_paper(client, message)
@@ -101,22 +106,16 @@ async def batch_extract_models_names(
 
             print(response.model_dump_json(indent=2))
 
-            models = [
-                m.name.value for m in response.extractions.models
-            ]
-            datasets = [
-                d.name.value for d in response.extractions.datasets
-            ]
-            libraries = [
-                f.name.value for f in response.extractions.libraries
-            ]
+            models = [m.name.value for m in response.extractions.models]
+            datasets = [d.name.value for d in response.extractions.datasets]
+            libraries = [f.name.value for f in response.extractions.libraries]
 
             data = [models, datasets, libraries]
 
 
 async def ignore_exceptions(
     client: instructor.client.Instructor | instructor.client.AsyncInstructor,
-    validation_set:List[Path]
+    validation_set: List[Path],
 ):
     for paper in validation_set:
         try:
@@ -124,7 +123,9 @@ async def ignore_exceptions(
         except bdb.BdbQuit:
             raise
         except Exception as e:
-            warnings.warn(f"Failed to extract paper informations from {paper.name}:\n{e}")
+            warnings.warn(
+                f"Failed to extract paper informations from {paper.name}:\n{e}"
+            )
 
 
 def main(argv=None):
@@ -132,21 +133,17 @@ def main(argv=None):
         prog=PROG,
         description=DESCRIPTION,
         epilog=EPILOG,
-        formatter_class=argparse.RawDescriptionHelpFormatter
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
-        "--papers",
-        nargs='*',
-        type=str,
-        default=None,
-        help="Papers to analyse"
+        "--papers", nargs="*", type=str, default=None, help="Papers to analyse"
     )
     parser.add_argument(
         "--input",
         metavar="TXT",
         type=Path,
         default=None,
-        help="List of papers to analyse"
+        help="List of papers to analyse",
     )
     options = parser.parse_args(argv)
 
@@ -162,16 +159,12 @@ def main(argv=None):
         papers = build_validation_set(ROOT_DIR / "data/")
         print(*papers, sep="\n")
 
-    if not all(map(lambda p:p.exists(), papers)):
-        papers = [
-            Path(ROOT_DIR / f"data/cache/arxiv/{paper}.txt") for paper in papers
-        ]
+    if not all(map(lambda p: p.exists(), papers)):
+        papers = [Path(ROOT_DIR / f"data/cache/arxiv/{paper}.txt") for paper in papers]
 
-    assert all(map(lambda p:p.exists(), papers))
+    assert all(map(lambda p: p.exists(), papers))
 
-    client = instructor.from_openai(
-        openai.AsyncOpenAI()
-    )
+    client = instructor.from_openai(openai.AsyncOpenAI())
 
     asyncio.run(ignore_exceptions(client, [paper.absolute() for paper in papers]))
 
