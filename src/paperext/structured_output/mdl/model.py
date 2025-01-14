7752c81f54b92cce41d5837e140750465a54f432
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2025-01-14 15:24:44 -0500

    Code review

--- src/paperext/structured_output/mdl/model.py
+++ src/paperext/structured_output/mdl/model.py
@@ -1,4 +1,9 @@
 # This model aims to extract Deep Learning Models, Datasets and Libraries from a
 # research paper
+# Very bad practice but we will allow it here since it acts like a proxy to the
+# latest model version and nothing else is imported
 from .model_v3 import *
+
+# * import does not import private variables
+# TODO: rename _FIRST_MESSAGE to FIRST_MESSAGE
 from .model_v3 import _FIRST_MESSAGE
