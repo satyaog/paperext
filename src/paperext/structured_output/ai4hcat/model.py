35c162439aa8e9402f8f3dda6b715eef901ef350
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2025-01-14 15:24:44 -0500

    Code review

--- src/paperext/structured_output/ai4hcat/model.py
+++ src/paperext/structured_output/ai4hcat/model.py
@@ -1,4 +1,9 @@
 # This model aims to extract Deep Learning Models, Datasets and Libraries from a
 # research paper
+# Very bad practice but we will allow it here since it acts like a proxy to the
+# latest model version and nothing else is imported
 from .model_v1 import *
+
+# * import does not import private variables
+# TODO: rename _FIRST_MESSAGE to FIRST_MESSAGE
 from .model_v1 import _FIRST_MESSAGE
