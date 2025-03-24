88e08d20695740fcef05f7df23b504e05f56f57e
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2025-03-24 10:57:29 -0400

    Add report on validation set

--- scripts/ai4h_create_validation_files.py
+++ scripts/ai4h_create_validation_files.py
@@ -1,30 +1,87 @@
+import csv
 import json
 from pathlib import Path
 
 from paperext.config import CFG
 from paperext.structured_output.utils import model_dump_yaml
 from paperext.utils import Paper
-from paperext.structured_output.ai4hcat.model import Category, Response, SubCategory
+from paperext.structured_output.ai4hcat.model import (
+    Category,
+    Explained,
+    Response,
+    SubCategory,
+)
 
 
-if __name__ == "__main__":
+def iter_categories(categories: list[str]):
+    pool = set()
+
+    for cat in categories:
+        try:
+            cat = Category(cat)
+
+        except ValueError:
+            assert cat.lower() in ("", "ko/check")
+            cat = Category("N/A")
+
+        if cat not in pool:
+            yield cat
+            pool.add(cat)
+
+    if not pool:
+        yield Category("N/A")
+
+
+def iter_subcategories(subcategories: list[str]):
+    pool = set()
+
+    for subcat in subcategories:
+        try:
+            subcat = SubCategory(subcat)
+
+        except ValueError:
+            assert subcat.lower() in ("", "ko/check")
+            subcat = SubCategory("N/A")
+
+        if subcat not in pool:
+            yield subcat
+            pool.add(subcat)
+
+    if not pool:
+        yield SubCategory("N/A")
+
+
+def main():
     paperoni = Path(CFG.dir.data / "paperoni-2022-01-01-2023-01-01-PR_2025-02-28.json")
-    _file = Path("ai4h_cat - validation_02.csv")
+    _file = Path(CFG.dir.data / "ai4hcat/export_02.csv")
 
-    data = {}
-    for line in _file.read_text().splitlines():
-        cat, cat_verif, subcat, subcat_verif, paper_id, *_ = line.split(",")
+    data: dict[str:dict] = {}
+    lines = list(csv.reader(_file.read_text().splitlines()))
+    while lines:
+        line = lines.pop(0)
+
+        if not lines[0][5]:
+            lines.pop(0)
+
+        _title, cat, cat_verif, subcat, subcat_verif, _paper_id, *_ = line
         assert not _
 
         if cat == "N/A" and subcat == "N/A":
             continue
 
-        if cat_verif != "OK" and cat_verif != cat:
-            cat = "N/A"
-        if subcat_verif != "OK" and subcat_verif != subcat:
-            subcat = "N/A"
+        if _paper_id:
+            paper_id = _paper_id
 
-        data[paper_id] = (cat, subcat)
+        if cat_verif == "OK":
+            cat_verif = cat
+        if subcat_verif == "OK":
+            subcat_verif = subcat
+
+        data.setdefault(paper_id, {"cat": [], "subcat": []})
+        if cat != "N/A" or not data[paper_id]["cat"]:
+            data[paper_id]["cat"].append(cat_verif)
+        if subcat != "N/A" or not data[paper_id]["subcat"]:
+            data[paper_id]["subcat"].append(subcat_verif)
 
     CFG.dir.merged.mkdir(exist_ok=True)
 
@@ -36,12 +93,16 @@ if __name__ == "__main__":
 
         assert paper.queries
 
-        cat, subcat = data.pop(paper._paper_id)
+        paper_data = data.pop(paper._paper_id)
+        categories = list(iter_categories(paper_data["cat"]))
+        subcategories = list(iter_subcategories(paper_data["subcat"]))
+
         response = paper.queries[0]
         analysis = Response.model_validate_json(response.read_text()).extractions
 
         if analysis.sustainable_development_is_central.value != (
-            cat != "N/A" or subcat != "N/A"
+            any(c != Category("N/A") for c in categories)
+            or any(sc != SubCategory("N/A") for sc in subcategories)
         ):
             analysis.sustainable_development_is_central.value = (
                 not analysis.sustainable_development_is_central.value
@@ -49,17 +110,42 @@ if __name__ == "__main__":
             analysis.sustainable_development_is_central.justification = ""
             analysis.sustainable_development_is_central.quote = ""
 
-        if analysis.primary_category.value != Category(cat):
-            analysis.primary_category.value = Category(cat)
+        if analysis.primary_category.value != categories[0]:
+            analysis.primary_category.value = categories[0]
             analysis.primary_category.justification = ""
             analysis.primary_category.quote = ""
-        if analysis.primary_sub_category.value != SubCategory(subcat):
-            analysis.primary_sub_category.value = SubCategory(subcat)
+        if analysis.primary_sub_category.value != subcategories[0]:
+            analysis.primary_sub_category.value = subcategories[0]
             analysis.primary_sub_category.justification = ""
             analysis.primary_sub_category.quote = ""
 
-        analysis.secondary_categories = []
-        analysis.secondary_sub_categories = []
+        analysis.secondary_categories += [
+            Explained(value=Category("N/A"), justification="", quote="")
+        ] * (len(categories[1:]) - len(analysis.secondary_categories))
+        analysis.secondary_sub_categories += [
+            Explained(value=SubCategory("N/A"), justification="", quote="")
+        ] * (len(subcategories[1:]) - len(analysis.secondary_sub_categories))
+
+        i = -1
+        for i, (prediction, valid) in enumerate(
+            zip(analysis.secondary_categories, categories[1:])
+        ):
+            if prediction.value != valid:
+                prediction.value = valid
+                prediction.justification = ""
+                prediction.quote = ""
+        analysis.secondary_categories[i + 1 :] = []
+
+        i = -1
+        for i, (prediction, valid) in enumerate(
+            zip(analysis.secondary_sub_categories, subcategories[1:])
+        ):
+            if prediction.value != valid:
+                prediction.value = valid
+                prediction.justification = ""
+                prediction.quote = ""
+        analysis.secondary_sub_categories[i + 1 :] = []
+
         analysis.applications = []
         analysis.new_primary_category.value = ""
         analysis.new_primary_category.justification = ""
@@ -76,3 +162,7 @@ if __name__ == "__main__":
         ).write_text(model_dump_yaml(analysis))
 
     assert not data
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/ai4h_validation_set_report.py
@@ -0,0 +1,104 @@
+import pandas as pd
+
+from paperext.config import CFG
+from paperext.structured_output.utils import model_validate_yaml
+from paperext.structured_output.ai4hcat.model import (
+    CATEGORISATION_TREE,
+    Category,
+    PaperExtractions,
+    SubCategory,
+    get_sub_categories,
+)
+
+
+pd.set_option("display.max_rows", None)  # Show all rows
+pd.set_option("display.max_columns", None)  # Show all columns
+pd.set_option("display.width", 1024)  # No line width limit
+pd.set_option("display.max_colwidth", None)  # Show full content of each cell
+pd.set_option("display.float_format", "{:.1f}".format)
+
+
+def main():
+    validation_set = CFG.dir.data / "ai4hcat/validation_set.txt"
+
+    objectives = {}
+    for cat in CATEGORISATION_TREE:
+        subcats = get_sub_categories(cat)
+
+        objectives[(cat, cat)] = {"objective": max(10, 3 * len(subcats)), "cnt": 0}
+
+        for subcat in subcats:
+            objectives[(cat, subcat)] = {
+                "objective": objectives[(cat, cat)]["objective"] / len(subcats),
+                "cnt": 0,
+            }
+
+    objectives.pop((Category("N/A").value, SubCategory("N/A").value))
+    objectives[(Category("N/A").value, SubCategory("N/A").value)] = {
+        "objective": 100,
+        "cnt": 0,
+    }
+
+    validation_files_objective = sum(
+        objectives[cat, cat]["objective"] for cat in CATEGORISATION_TREE
+    )
+    validation_files_cnt = 0
+    for validation_file in validation_set.read_text().splitlines():
+        validation_file = CFG.dir.merged / f"{validation_file}.yaml"
+
+        validation: PaperExtractions = model_validate_yaml(
+            PaperExtractions, validation_file.read_text()
+        )
+
+        validation_files_cnt += 1
+
+        categories = set()
+        categories_pairs = set()
+        for cat in [validation.primary_category, *validation.secondary_categories]:
+            cat = cat.value
+            categories.add(cat.value)
+
+            for subcat in [
+                validation.primary_sub_category,
+                *validation.secondary_sub_categories,
+            ]:
+                subcat = subcat.value
+
+                if subcat == SubCategory("N/A"):
+                    continue
+
+                if (cat.value, subcat.value) in objectives:
+                    categories_pairs.add((cat.value, subcat.value))
+
+        if len(categories) >= 2 and Category("N/A").value in categories:
+            categories.remove(Category("N/A").value)
+
+        for cat in categories:
+            objectives[(cat, cat)]["cnt"] += 1
+
+        for pair in categories_pairs:
+            objectives[pair]["cnt"] += 1
+
+    report = pd.DataFrame(objectives).transpose()
+    report = pd.concat(
+        [
+            report,
+            pd.DataFrame(
+                {
+                    ("validation_files_cnt", ""): {
+                        "objective": validation_files_objective,
+                        "cnt": validation_files_cnt,
+                    }
+                }
+            ).transpose(),
+        ]
+    )
+
+    report = report.assign(missing=report.objective - report.cnt)
+    report.loc[report["missing"] < 0, "missing"] = 0.0
+
+    print(report)
+
+
+if __name__ == "__main__":
+    main()
