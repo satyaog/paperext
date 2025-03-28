15dc8acbcaacb01573e3bb717a00433a34bed502
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2025-03-28 15:44:14 -0400

    _ 6c41cee7c7dc62c5e502c639649ce4241fb08fff

--- scripts/ai4h_create_validation_files.py
+++ scripts/ai4h_create_validation_files.py
@@ -57,10 +57,10 @@ def iter_subcategories(subcategories: list[str]):
 def main():
     paperoni = (
         # None
-        Path(CFG.dir.data / "paperoni-2022-01-01-2023-01-01-PR_2025-02-28.json")
-        # Path(CFG.dir.data / "paperoni-2022-01-01-2023-01-01_2025-03-01.json")
+        # Path(CFG.dir.data / "paperoni-2022-01-01-2023-01-01-PR_2025-02-28.json")
+        Path(CFG.dir.data / "paperoni-2022-01-01-2023-01-01_2025-03-01.json")
     )
-    _file = Path(CFG.dir.data / "ai4hcat/export_03.csv")
+    _file = Path(CFG.dir.data / "ai4hcat/export_04.csv")
 
     data: dict[str:dict] = {}
     lines = list(csv.reader(_file.read_text().splitlines()))
